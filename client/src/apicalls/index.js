2dee55a5f94cf4de6fe34f9b38ba0abd8af341be
smgeventz <smgeventztest@gmail.com>
2024-09-20 09:28:11 +0530

    Update index.js

--- client/src/apicalls/index.js
+++ client/src/apicalls/index.js
@@ -1,13 +1,9 @@
 import axios from 'axios';
 
-// Create an axios instance with the base URL pointing to your backend
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000', // Fallback to localhost:5000 during development
-  headers: {
-    Authorization: localStorage.getItem('token')
-      ? `Bearer ${localStorage.getItem('token')}`
-      : '', // Set the token if it exists
-  },
+    headers: {
+         Authorization : `Bearer ${localStorage.getItem('token')}`
+    }
 });
 
 export default axiosInstance;
